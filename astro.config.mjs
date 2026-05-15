009dd6b1e300275311c62b9a55865de16c1d358c
LUIS <elleon2100@gmail.com>
2026-05-15 08:41:56 -0400

    correccion de errores de navegacion movil y about

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://wattlogic.io',
+  site: 'https://wattlogic.site',
   output: 'static',
   trailingSlash: 'never',
   integrations: [
